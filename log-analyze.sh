6c3d3f9c0e38075cccf98d26aba695567cdbbf6b
viktorhex <christopherdillinger200@gmail.com>
2025-02-18 08:11:18 -0500

    update

--- log-analyze.sh
+++ log-analyze.sh
@@ -4,7 +4,7 @@ most_frequent_ip_addresses(){
   grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' $1 \
     | awk '{for (i = 1; i<= NF; i++) word[$i]++} END {for (w in word) print word[w], w}' \
     | sort -rn \
-    | awk '{printf "%-20s %-20s\n", $2, $1}' \
+    | awk '{printf "%-20s %s requests\n", $2, $1}' \
     | awk 'NR <= 5'
 }
 
