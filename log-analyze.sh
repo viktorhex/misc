63c050ec3410626ba492f17e60bd8e7a50e9e83d
viktorhex <christopherdillinger200@gmail.com>
2025-02-18 08:16:31 -0500

    update

--- log-analyze.sh
+++ log-analyze.sh
@@ -2,10 +2,24 @@
 
 most_frequent_ip_addresses(){
   grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' $1 \
-    | awk '{for (i = 1; i<= NF; i++) word[$i]++} END {for (w in word) print word[w], w}' \
-    | sort -rn \
-    | awk '{printf "%-20s %s requests\n", $2, $1}' \
+    | get_freq_and_sort \
+    | get_top_5_formatted
+}
+
+get_freq_and_sort(){
+  awk '{for (i = 1; i<= NF; i++) word[$i]++} END {for (w in word) print word[w], w}' \
+    | sort -rn
+}
+
+get_top_5_formatted(){
+  awk '{printf "%-25s %s requests\n", $2, $1}' \
     | awk 'NR <= 5'
+}
+
+most_frequent_paths(){
+  grep -oP '"\w+ \K[^?"]+?(?=\s+HTTP)' $1 \
+    | get_freq_and_sort \
+    | get_top_5_formatted
 }
 
 #!/bin/bash
@@ -18,8 +32,11 @@ if [[ "$1" == "--input="* ]]; then
 
         # Main execution
         echo "========== Log Analysis =========="
-        echo -e "Most frequent IP adresses:"
+        echo -e "\nMost frequent IP adresses:"
         most_frequent_ip_addresses $INPUT
+
+        echo -e "\nMost frequent paths:"
+        most_frequent_paths $INPUT
 
     else
         echo "Error: The directory '$INPUT' does not exist."
